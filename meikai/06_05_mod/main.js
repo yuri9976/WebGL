6802e1a348f2cb6ca2af4287028ea200617d70a6
yuriyoshida <yuri9976@gmail.com>
2018-03-02 09:03:33 +0900

    Add files via upload

--- meikai/06_05_mod/main.js
+++ meikai/06_05_mod/main.js
@@ -2,10 +2,6 @@ window.onload = function(){
 	// HTMLからCanvas要素を取得
 	var c = document.getElementById('canvas');
 
-	// Canvasのサイズを指定
-	c.width = 512;
-	c.height = 512;
-
 	// CanvasエレメントからWebGLを取得
 	var gl = c.getContext('webgl');
 
@@ -15,8 +11,11 @@ window.onload = function(){
 		return;
 	}
 
+	// canvas のマウスムーブイベントに処理を登録
+	c.addEventListener('click', mouseMove, true);
+
 	// Canvasをクリア
-	gl.clearColor(0.3, 0.3, 0.3, 1.0);
+	gl.clearColor(0.5, 0.5, 0.5, 1.0);
 	gl.clearDepth(1.0);
 
 	// VSとFSを生成
@@ -26,15 +25,15 @@ window.onload = function(){
 	// VSとFSをプログラムに紐づけ
 	var programs = shaderProgram(vertexSource, fragmentSource);
 
-	// 【New】uniformロケーション情報を取得する
+	// ライトベクトル
+	var lightDirection = [1.0, 1.0, 1.0];
+	
+	// uniformロケーション情報を取得する
 	var uniLocation = {};
 	uniLocation.mvpMatrix = gl.getUniformLocation(programs, 'mvpMatrix');
 	uniLocation.invMatrix = gl.getUniformLocation(programs, 'invMatrix');
 	uniLocation.lightDirection = gl.getUniformLocation(programs, 'lightDirection');
 
-	// ライトベクトル
-	var lightDirection = [1.0, 1.0, 1.0];
-	
 	// 行列
 	var mat = new matIV();
 	var mMatrix = mat.identity(mat.create());
@@ -42,11 +41,11 @@ window.onload = function(){
 	var pMatrix = mat.identity(mat.create());
 	var vpMatrix = mat.identity(mat.create());
 	var mvpMatrix = mat.identity(mat.create());
-	// 【New】逆行列
+	// 逆行列
 	var invMatrix = mat.identity(mat.create());
 
 	// カメラ設定
-	var cameraPosition = [0.0, 0.0, 3.0]; // z方向にオフセット
+	var cameraPosition = [0.0, 0.0, 5.0]; // z方向にオフセット
 	var centerPoint = [0.0, 0.0, 0.0];    // 原点はゼロ
 	var cameraUp = [0.0, 1.0, 0.0];       // y方向
 	mat.lookAt(cameraPosition, centerPoint, cameraUp, vMatrix);
@@ -62,24 +61,22 @@ window.onload = function(){
 	mat.multiply(pMatrix, vMatrix, vpMatrix);
 
 	var count = 0;
-	// 【New】深度テストを有効化
+	// 深度テストを有効化
 	gl.enable(gl.DEPTH_TEST);
-	// 【New】深度テストの方法を指定
+	// 深度テストの方法を指定
 	gl.depthFunc(gl.LEQUAL);
 
 	// 球体を生成（楕円体にしている）
 	var sphereData = sphere(32, 32, 1.0);
 	
-	var coneData1 = cone(1,-0.05);
-	var coneData2 = cone(-1,-0.05);
+	var coneData1 = cone(1,-1);
+	var coneData2 = cone(-1,-1);
 
 	render();
 
 	function render(){
-
 		count++;
-
-		var radians = (count % 360) * Math.PI / 180;
+		var radians = (count % 180) * Math.PI / 360;
 
 		// Canvasをクリアする（クリア対象：カラーバッファのみ）
 		//gl.clear(gl.COLOR_BUFFER_BIT);
@@ -89,21 +86,25 @@ window.onload = function(){
 		mat.identity(mMatrix);
 
 		var axis = [0.0, 1.0, 0.0];
-		mat.rotate(mMatrix, radians, axis, mMatrix);
+//		mat.rotate(mMatrix, radians, axis, mMatrix);
 
 		mat.multiply(vpMatrix, mMatrix, mvpMatrix);
 		mat.inverse(mMatrix, invMatrix);
 
-		// 頂点データから頂点座標位置のVBOを生成
+		// 体頂点データから頂点座標位置のVBOを生成
 		var vPositionBuffer = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, vPositionBuffer);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sphereData.p), gl.STATIC_DRAW);
-		// VBO（プログラムオブジェクトに頂点データを登録）
+		// VBO（プログラムオブジェクトに体頂点データを登録）
 		var attLocPosition = gl.getAttribLocation(programs, 'position');
 		gl.enableVertexAttribArray(attLocPosition);
 		gl.vertexAttribPointer(attLocPosition, 3, gl.FLOAT, false, 0, 0);
 
-		// 頂点データから法線VBOを生成して登録
+		// コンパイルは通るけどsphereがでなくなる('・ω・`）
+//		var vPositionBuffer = makeVertexVBO(sphereData.p);
+//		var attLocPosition = registerVBO();
+
+		// 体頂点データから法線VBOを生成して登録
 		var vNormalBuffer = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, vNormalBuffer);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sphereData.n), gl.STATIC_DRAW);
@@ -111,7 +112,7 @@ window.onload = function(){
 		gl.enableVertexAttribArray(attLocNormal);
 		gl.vertexAttribPointer(attLocNormal, 3, gl.FLOAT, false, 0, 0);
 
-		// 頂点データから頂点色のVBOを生成
+		// 体頂点データから頂点色のVBOを生成
 		var vColorBuffer = gl.createBuffer(); 
 		gl.bindBuffer(gl.ARRAY_BUFFER, vColorBuffer);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sphereData.c), gl.STATIC_DRAW);
@@ -119,41 +120,33 @@ window.onload = function(){
 		gl.enableVertexAttribArray(attLocColor);
 		gl.vertexAttribPointer(attLocColor, 4, gl.FLOAT, false, 0, 0);
 	
-		// インデックスバッファの生成
+		// 体頂点インデックスバッファの生成
 		var indexBuffer = gl.createBuffer();
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(sphereData.i), gl.STATIC_DRAW);
+
+//		Texture texture = context.createTexture();
+//		context.bindTexture(TEXTURE_2D, texture);
+
 
 		// シェーダにuniformデータを送る
 		gl.uniformMatrix4fv(uniLocation.mvpMatrix, false, mvpMatrix);
 		gl.uniformMatrix4fv(uniLocation.invMatrix, false, invMatrix);
 		gl.uniform3fv(uniLocation.lightDirection, lightDirection);
 
-		// uniform変数のロケーションを取得する
-		//var uniLocation = gl.getUniformLocation(programs, 'mvpMatrix');
-		//gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-
-		// ライトの位置を指定する
-		//var lightPosition = [1.0, 1.0, 1.0];
-		
-		// uniformロケーション情報を取得
-		//var uniLocation = gl.getUniformLocation(programs, 'lightPosition');
-		// シェーダにライトの位置を送信
-		//gl.uniform3fv(uniLocation, lightPosition);
-		
 		//  インデックスバッファを用いるのでdrawArrayではなくdrawElements
 		gl.drawElements(gl.TRIANGLES, sphereData.i.length, gl.UNSIGNED_SHORT, 0);
 
-		// 頂点データから頂点座標位置のVBOを生成
+		// 足１頂点データから頂点座標位置のVBOを生成
 		var vPositionBuffer2 = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, vPositionBuffer2);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coneData1.vertices), gl.STATIC_DRAW);
-		// VBO（プログラムオブジェクトに頂点データを登録）
+		// VBO（プログラムオブジェクトに足１頂点データを登録）
 		var attLocPosition2 = gl.getAttribLocation(programs, 'position');
 		gl.enableVertexAttribArray(attLocPosition2);
 		gl.vertexAttribPointer(attLocPosition2, 3, gl.FLOAT, false, 0, 0);
 
-		// 頂点データから法線VBOを生成して登録
+		// 足１頂点データから法線VBOを生成して登録
 		var vNormalBuffer2 = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, vNormalBuffer2);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coneData1.normals), gl.STATIC_DRAW);
@@ -161,7 +154,7 @@ window.onload = function(){
 		gl.enableVertexAttribArray(attLocNormal2);
 		gl.vertexAttribPointer(attLocNormal2, 3, gl.FLOAT, false, 0, 0);
 
-		// 頂点データから頂点色のVBOを生成
+		// 体頂点データから足１頂点色のVBOを生成
 		var vColorBuffer2 = gl.createBuffer(); 
 		gl.bindBuffer(gl.ARRAY_BUFFER, vColorBuffer2);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sphereData.c), gl.STATIC_DRAW);
@@ -169,7 +162,7 @@ window.onload = function(){
 		gl.enableVertexAttribArray(attLocColor2);
 		gl.vertexAttribPointer(attLocColor2, 4, gl.FLOAT, false, 0, 0);
 		
-		// インデックスバッファの生成
+		// 足１頂点インデックスバッファの生成
 		var indexBuffer2 = gl.createBuffer();
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer2);
 		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(coneData1.indices), gl.STATIC_DRAW);
@@ -181,16 +174,16 @@ window.onload = function(){
 
 		gl.drawElements(gl.TRIANGLES, coneData1.indices.length, gl.UNSIGNED_SHORT, 0);
 
-		// 頂点データから頂点座標位置のVBOを生成
+		// 足２頂点データから頂点座標位置のVBOを生成
 		var vPositionBuffer3 = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, vPositionBuffer3);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coneData2.vertices), gl.STATIC_DRAW);
-		// VBO（プログラムオブジェクトに頂点データを登録）
+		// VBO（プログラムオブジェクトに足２頂点データを登録）
 		var attLocPosition2 = gl.getAttribLocation(programs, 'position');
 		gl.enableVertexAttribArray(attLocPosition2);
 		gl.vertexAttribPointer(attLocPosition2, 3, gl.FLOAT, false, 0, 0);
 
-		// 頂点データから法線VBOを生成して登録
+		// 足２頂点データから法線VBOを生成して登録
 		var vNormalBuffer2 = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, vNormalBuffer2);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coneData2.normals), gl.STATIC_DRAW);
@@ -198,7 +191,7 @@ window.onload = function(){
 		gl.enableVertexAttribArray(attLocNormal2);
 		gl.vertexAttribPointer(attLocNormal2, 3, gl.FLOAT, false, 0, 0);
 
-		// 頂点データから頂点色のVBOを生成
+		// 体頂点データから足２頂点色のVBOを生成
 		var vColorBuffer2 = gl.createBuffer(); 
 		gl.bindBuffer(gl.ARRAY_BUFFER, vColorBuffer2);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sphereData.c), gl.STATIC_DRAW);
@@ -206,10 +199,12 @@ window.onload = function(){
 		gl.enableVertexAttribArray(attLocColor2);
 		gl.vertexAttribPointer(attLocColor2, 4, gl.FLOAT, false, 0, 0);
 		
-		// インデックスバッファの生成
+		// 足２頂点インデックスバッファの生成
 		var indexBuffer2 = gl.createBuffer();
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer2);
 		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(coneData2.indices), gl.STATIC_DRAW);
+
+		mat.lookAt(cameraPosition, centerPoint, cameraUp, vMatrix);
 
 		// シェーダにuniformデータを送る
 		gl.uniformMatrix4fv(uniLocation.mvpMatrix, false, mvpMatrix);
@@ -256,6 +251,32 @@ window.onload = function(){
 		// VSとFSを紐づけたプログラムを返す
 		return programs;
 	}
+
+	// 繰り返し処理をまとめてみるテスト
+	function makeVertexVBO(indecies){
+		var vPositionBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, vPositionBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(indecies.p), gl.STATIC_DRAW);
+		return vPositionBuffer;
+	}
+	// 繰り返し処理をまとめてみるテスト
+	function registerVBO(){
+		var attLocPosition = gl.getAttribLocation(programs, 'position');
+		gl.enableVertexAttribArray(attLocPosition);
+		gl.vertexAttribPointer(attLocPosition, 3, gl.FLOAT, false, 0, 0);
+		return attLocPosition;
+	}
+
+	function mouseMove(e) {
+		var c = document.getElementById('canvas');
+		var x = (e.offsetX / c.width - 0.5) * 4.0;
+		var y = - (e.offsetY / c.height - 0.5) * 4.0;
+		cameraPosition = [x, y, 1.0];
+		mat.lookAt(cameraPosition, centerPoint, cameraUp, vMatrix);
+		mat.multiply(pMatrix, vMatrix, vpMatrix);
+		console.log("x = %d, y = %d", x, y);
+	}
+
 };
 
 
